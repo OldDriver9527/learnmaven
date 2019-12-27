5a96b2a95808dbbf8a772f1f3b973636a7f8ed93
OldDriver9527 <1187557502@qq.com>
2019-12-27 17:35:08 +0800

    试试

--- src/main/java/org/olddriver/LearnMaven.java
+++ src/main/java/org/olddriver/LearnMaven.java
@@ -4,9 +4,9 @@ public class LearnMaven {
     /*
     1.maven介绍
     maven是java项目管理工具，可以用于构建项目，管理依赖，管理项目信息
-    maven使用POM构建项目，POM在pom.xml中定义，POM是maven的基本工作单元
+    POM是maven的基本工作单元，maven使用POM构建项目，POM在pom.xml中定义
     2.项目构建生命周期
-    maven将项目构建的生命周期总结为三套，clean，default，site，每个生命周期分为多个阶段，靠后的阶段依赖于靠前的阶段
+    maven将项目构建总结为三套独立生命周期，clean，default，site，每个生命周期分为多个阶段，靠后的阶段依赖于靠前的阶段
     ----------------------------------------------------------------------------
     clean生命周期负责清理项目，包含pre-clean，clean，post-clean等阶段
     通过clean命令调用clean生命周期中clean阶段，执行clean阶段前的所有阶段，将target目录删除,清理构建数据
@@ -24,28 +24,28 @@ public class LearnMaven {
     生命周期阶段与插件目标绑定
     通常不通过命令调用具有连字符的阶段
     3.POM
-    POM是maven的基本工作单元，定义在pom.xml中
+    POM指项目对象模型，是maven的基本工作单元，定义在pom.xml中
     父POM
     maven中存在默认父POM，所有POM默认继承父POM
     POM继承
-    在子POM中声明其父POM，POM继承目的在于消除重复配置，父POM打包方式必须是pom
-    子POM会继承父POM中的依赖列表，
+    在子POM中声明其父POM，子POM会继承父POM的部分配置，如依赖列表
+    POM继承目的在于消除重复配置，父POM打包方式必须是pom
     POM聚合
-    在父POM中声明其子模块，对父模块的命令同时也会应用于子模块，父POM打包方式必须是pom
+    在父POM中声明作为其模块的子POM，对父POM的命令同时也会应用于子POM，父POM打包方式必须是pom
     4.依赖机制
-    依赖坐标
-    依赖坐标由groupId，artifactId，version，packaging，classifier构成
+    maven坐标
+    maven通过坐标定位构件，maven坐标由groupId，artifactId，version，packaging，classifier构成
     ----------------------------------------------------------------------------
     使用外部依赖
     dependencies元素中声明所有外部依赖，外部依赖至少指明groupId，artifactId，version，scope，
     还可以定义type，optional，exclusions
     type指明依赖类型，默认值为jar
-    optional标记依赖是否可选
+    optional标记依赖是否可选？？？
     exclusions用于排除传递性依赖
     maven优先在本地仓库寻找依赖，若本地仓库中不存在，maven从远程仓库下载依赖存储在本地仓库中
     ----------------------------------------------------------------------------
     依赖范围
-    maven在编译，测试，运行时使用三套独立的classpath，依赖范围用于控制依赖与三套classpath的关系
+    maven在编译，测试，运行项目时使用三套独立的classpath，依赖范围用于控制依赖与三套classpath的关系
     存在6种依赖范围，compile，provided，runtime，test，system，import
     compile
     compile范围依赖在所有classpath中均可用，默认值
@@ -70,9 +70,14 @@ public class LearnMaven {
     provided           provided     不传递     provided    provided
     runtime            runtime      不传递     不传递       runtime
     ----------------------------------------------------------------------------
+    可选依赖
+    optional元素定义是否为可选依赖，可选依赖仅在所在POM中有效，不会作为传递依赖进行传递
+    排除依赖
+    exclusions元素用于排除指定依赖中的依赖，不作为传递依赖引入
+    ----------------------------------------------------------------------------
     依赖管理
     依赖管理用于集中依赖信息
-    使用场景之一在父POM中使用dependencyManagement元素定义依赖完整信息，在dependencyManagement中声明的依赖不会引入项目
+    使用场景之一在父POM中使用dependencyManagement元素定义依赖完整信息（在dependencyManagement中声明的依赖不会引入项目）
     在子POM中使用dependencies元素简单引用依赖，通过groupId, artifactId, type, classifier与dependencyManagement中依赖进行匹配
     type默认值为jar，classifier默认值为null
     导入依赖
@@ -81,7 +86,11 @@ public class LearnMaven {
     ----------------------------------------------------------------------------
     BOM bill of materials
     BOM是特殊的POM，专门用于被其他POM导入dependencyManagement配置
-    5.pom.xml
+    5.maven仓库
+    maven仓库分为本地仓库，远程仓库
+    远程仓库分为中央仓库，私服，其他公共库
+    中央仓库指maven核心远程仓库
+    6.pom.xml
     <project>
     pom.xml中根元素
     <project><modelVersion>
